ad95d11ca8214e07b168a00540cc2ffe32d01a06
Matias Piipari <matias.piipari@gmail.com>
2018-03-18 01:05:44 +0000

    A more complete example that is panicking.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,15 @@ extern crate serde_json;
 extern crate rocket;
 extern crate rocket_contrib;
 
+extern crate hyper;
+extern crate futures;
+extern crate tokio_core;
+
+use hyper::{Request, Method, Client, Uri};
+use hyper::header::{ContentLength, ContentType};
+use futures::{Future, Stream};
+use tokio_core::reactor::Core;
+
 //use std::fs::File;
 //use std::io::Read;
 //use std::string;
@@ -17,19 +26,40 @@ mod gitlab;
 mod hangoutchat;
 
 #[post("/gitlab/webhook", format = "application/json", data = "<payload>")]
-fn hello(payload: Json<gitlab::WebhookPayload>) -> Json<hangoutchat::TextMessagePayload> {
+fn convert_gitlab_webhook(payload: Json<gitlab::WebhookPayload>) -> Json<hangoutchat::TextMessagePayload> {
     let msg = payload.into_inner();
     match msg.object_kind.as_ref() {
         "issue" => println!("Issue: {}", msg.object_kind),
         _ => println!("Unexpected payload type: ${:?}", msg),
     }
 
-    let outgoing_msg = hangoutchat::TextMessagePayload { text: "foo".to_string() };
-    return Json(outgoing_msg);
+    let space = "AAAA0gIpT4c";
+    let key = "AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI";
+    let token = "6LhzSUAX7eEjfR745yIHyydtoSXhlmD0PGKHlDTfOec%3D";
+    let url: Uri = format!("https://chat.googleapis.com/v1/spaces/${}/messages?key={}&token={}", space, key, token).parse().unwrap();
+
+    let outgoing_msg_payload = hangoutchat::TextMessagePayload { text: "foo".to_string() };
+    let outgoing_msg = serde_json::to_string(&outgoing_msg_payload).unwrap();
+
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+    let mut req = Request::new(Method::Post, url);
+
+    req.headers_mut().set(ContentType::json());
+    req.headers_mut().set(ContentLength(outgoing_msg.len() as u64));
+    req.set_body(outgoing_msg);
+
+    let post = client.request(req).map(|res| {
+        println!("POST: {}", res.status());
+    });
+
+    core.run(post).unwrap();
+    
+    return Json(outgoing_msg_payload);
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![convert_gitlab_webhook]).launch();
 }
 
 #[cfg(test)]
