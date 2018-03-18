73fe818f39ed5f574071962f7f0dbb28119d4e98
Matias Piipari <matias.piipari@gmail.com>
2018-03-18 11:33:24 +0000

    Separating message sending to its own module.

--- src/hangoutchat.rs
+++ src/hangoutchat.rs
@@ -2,3 +2,52 @@
 pub struct TextMessage {
     pub text: String 
 }
+
+use hyper::{Client, Request, Method, Uri};
+use hyper_tls::{HttpsConnector};
+use hyper::header::{ContentLength, ContentType};
+use futures::{Future};
+use tokio_core::reactor::Core;
+
+use serde_json;
+use num_cpus;
+
+pub struct Sender {
+    pub space: String,
+    pub key: String,
+    pub token: String
+}
+
+impl Sender {
+    fn url(&self) -> Uri {
+        return format!("https://chat.googleapis.com/v1/spaces/{}/messages?key={}&token={}", self.space, self.key, self.token).parse().unwrap();
+    }
+    
+    pub fn send(&self, outgoing_msg_payload: TextMessage) {
+        let outgoing_msg = serde_json::to_string(&outgoing_msg_payload).unwrap();
+        let mut req = Request::new(Method::Post, self.url());
+
+        req.headers_mut().set(ContentType::json());
+        req.headers_mut().set(ContentLength(outgoing_msg.len() as u64));
+
+        println!("Outgoing message: {}", outgoing_msg);
+        
+        req.set_body(outgoing_msg);
+
+        let mut core = Core::new().unwrap();
+        let client = Client::configure()
+                                    .connector(HttpsConnector::new(num_cpus::get(), &core.handle()).unwrap())
+                                    .build(&core.handle());
+
+        let post = client.request(req).map(|res| {
+            println!("POST: {}", res.status());
+        });
+
+        match core.run(post) {
+            Ok(response) => println!("Result was alright: {:?}", response),
+            Err(e) => {
+                println!("Error occurred: {}", e);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,11 @@ extern crate serde_json;
 extern crate rocket;
 extern crate rocket_contrib;
 
-extern crate hyper;
-extern crate hyper_tls;
 extern crate futures;
 extern crate tokio_core;
 extern crate num_cpus;
-
-use hyper::{Client, Request, Method, Uri};
-use hyper_tls::{HttpsConnector};
-use hyper::header::{ContentLength, ContentType};
-use futures::{Future};
-use tokio_core::reactor::Core;
-
+extern crate hyper;
+extern crate hyper_tls;
 use rocket_contrib::Json;
 
 mod gitlab;
@@ -43,44 +36,13 @@ fn gitlab_webhook_to_hangout_message(payload: gitlab::WebhookPayload) -> hangout
 
 #[post("/gitlab/webhook", format = "application/json", data = "<payload>")]
 fn convert_gitlab_webhook(payload: Json<gitlab::WebhookPayload>) {
-    let msg = payload.into_inner();
-    match msg.object_kind.as_ref() {
-        "issue" => println!("Issue: {}", msg.object_kind),
-        _ => println!("Unexpected payload type: ${:?}", msg),
-    }
+    let sender = hangoutchat::Sender {
+        space: "AAAA0gIpT4c".to_string(),
+        key: "AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI".to_string(),
+        token: "6LhzSUAX7eEjfR745yIHyydtoSXhlmD0PGKHlDTfOec%3D".to_string()
+    };
 
-    let space = "AAAA0gIpT4c";
-    let key = "AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI";
-    let token = "6LhzSUAX7eEjfR745yIHyydtoSXhlmD0PGKHlDTfOec%3D";
-    let url: Uri = format!("https://chat.googleapis.com/v1/spaces/{}/messages?key={}&token={}", space, key, token).parse().unwrap();
-
-    let outgoing_msg_payload = gitlab_webhook_to_hangout_message(msg);
-    let outgoing_msg = serde_json::to_string(&outgoing_msg_payload).unwrap();
-
-    let mut core = Core::new().unwrap();
-    let client = Client::configure()
-                        .connector(HttpsConnector::new(num_cpus::get(), &core.handle()).unwrap())
-                        .build(&core.handle());
-
-    let mut req = Request::new(Method::Post, url);
-
-    req.headers_mut().set(ContentType::json());
-    req.headers_mut().set(ContentLength(outgoing_msg.len() as u64));
-
-    println!("Outgoing message: {}", outgoing_msg);
-    
-    req.set_body(outgoing_msg);
-
-    let post = client.request(req).map(|res| {
-        println!("POST: {}", res.status());
-    });
-
-    match core.run(post) {
-        Ok(response) => println!("Result was alright: {:?}", response),
-        Err(e) => {
-            println!("Error occurred: {}", e);
-        }
-    }
+    sender.send(gitlab_webhook_to_hangout_message(payload.into_inner()));
 }
 
 fn main() {
